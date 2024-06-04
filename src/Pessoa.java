9bf3cf66b68a9dc2e065f5ef11ee36bd46affc52
Pâmella Maria <pamellamariadev@gmail.com>
2024-06-04 13:56:56 -0300

    Update Pessoa.java

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -6,4 +6,14 @@ public abstract class Pessoa {
         this.nome = nome;
         this.idade = idade;
     }
+    public String getNome() {
+        return nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    @Override
+    public String toString() {
+        return "Nome: " + nome + ", Idade: " + idade;
+    }
 }
